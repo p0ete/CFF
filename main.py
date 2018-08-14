00a6b0cb3924f5548b2bab3544d4336e5b4dc9e3
p0ete <pauline.reiff@live.fr>
2018-08-14 11:57:19 +0200

    Génère toutes les solutions possibles
    Il faut encore:
    - faire des combinaisons de toutes les solutions possibles (par service_intention_id)
    - coder l'objective function pour choisir la meilleure solution

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ import datetime
 import isodate
 
 from route_graph import *
-# import utils.translate as translate
-# from utils.validate_solution import *
 
+## Depth First Search algorithm (pour trouver tous les paths possibles dans le réseau) 
+def myDFS(trans_dict,start,done,paths,path=[]): 
+	path=path+[start] 
+	# if len(path)==length:
+	# 	paths.append(path) 
+	# else:
+	# if len(trans_dict[start]) == 0:
+	# 	paths.append(path) 
+	if start not in trans_dict.keys():
+		paths.append(path)
+	else:
+		# if len(trans_dict[start]) == 0:
+		# 	done = True
+		# else:
+		for node in trans_dict[start]:
+			a = node['node']
+			myDFS(trans_dict,a,done,paths,path)
 
 ## Fonction retourne la section avec le temps de parcours minimum
 def le_plus_court(sections):
@@ -20,158 +35,243 @@ def le_plus_court(sections):
 	return result
 
 ## Fonction qui retourne un dict avec le 'entry_time' et le 'exit_time' pour la section selectionnée
-def compute_time(old_time, next_section):
-	exit = datetime.datetime.strptime(old_time['exit_time'], "%H:%M:%S") + \
-	 isodate.parse_duration(next_section['minimum_running_time'])
-	
+def compute_time(section_requirements, entry_time, section):
+	minimum_running_time = isodate.parse_duration(section['minimum_running_time'])
+	# print(entry_time)
+	# print(minimum_running_time)
+	exit_time = entry_time +  minimum_running_time
+	# print(section_requirements)
+	if section_requirements != None:
+		## First check if there are any time requirement 
+		if "entry_latest" in section_requirements.keys():
+			entry_latest = datetime.datetime.strptime(section_requirements["entry_latest"], "%H:%M:%S")
+			if entry_time > entry_latest:
+				print("The entry time of the train in station " + str(section_requirements["section_marker"]) + " is to late.")
+				pass
+			else:
+				print("The entry_latest requirement is respected.")
+
+		if "entry_earliest" in section_requirements.keys():
+			entry_earliest = datetime.datetime.strptime(section_requirements["entry_earliest"], "%H:%M:%S")
+			if entry_time < entry_earliest:
+				print("The entry time of the train in station " + str(section_requirements["section_marker"]) + " is to early.")
+				pass
+			else:
+				print("The entry_earliest requirement is respected.")
+
+		if "min_stopping_time" in section_requirements.keys():
+			exit_time += isodate.parse_duration(section_requirements['min_stopping_time'])
+			print("The min_stopping_time of the train in station " + str(section_requirements["section_marker"]) + " is respected.")
+
+
+		if "exit_earliest" in section_requirements.keys():
+			exit_earliest = datetime.datetime.strptime(section_requirements["exit_earliest"], "%H:%M:%S")
+			if exit_time < exit_earliest:
+				exit_time = exit_earliest
+			print("The exit_earliest of the train in station " + str(section_requirements["section_marker"]) + " is respected.")
+
+
+		if "exit_latest" in section_requirements.keys():
+			exit_latest = datetime.datetime.strptime(section_requirements["exit_latest"], "%H:%M:%S")
+			if exit_time > exit_latest:
+				print("The exit_latest in station " + str(section_requirements["section_marker"]) + " cannot be respected.")
+				pass
+			else:
+				print("The exit_latest in station " + str(section_requirements["section_marker"]) + " is respected.")
+
 	time = {
-		"entry_time": datetime.datetime.strptime(old_time['exit_time'], "%H:%M:%S").strftime("%H:%M:%S"),
-		"exit_time": exit.strftime("%H:%M:%S")
+		"entry_time": entry_time.strftime("%H:%M:%S"),
+		"exit_time": exit_time.strftime("%H:%M:%S")
 	}
+
 	return time
 
+## Représente le réseau de noeuds sous la forme d'un dict
+def generate_paths_dict(scenario_content):
+	result = {}
+	for route in scenario_content["routes"]:
+		for path in route["route_paths"]:
+			for (i, route_section) in enumerate(path["route_sections"]):
+				from_node = from_node_id(path, route_section, i)[1:-1]
+				to_node = to_node_id(path, route_section, i)[1:-1]
+				sn = route_section['sequence_number']
+
+				if from_node not in result.keys():
+					result[from_node] = []
+
+				doublon = False
+				for k in range(len(result[from_node])):
+					if result[from_node][k] == {'node': to_node, 'seq_nb': sn}:
+						doublon = True
+				if not doublon:
+					result[from_node].append({'node': to_node, 'seq_nb': sn})
+
+				# print("Adding Edge from {} to {} with sequence number {}".format(from_node_id(path, route_section, i), to_node_id(path, route_section, i), sn))
+	# print(result)
+	return result
+
+## Converti le chemin de nodes en chemin de seq
+def convert_paths(paths, the_route_net):
+	result = []
+	for path in paths:
+		new_path = []
+		for (i, node) in enumerate(path):
+			if node in the_route_net.keys():
+				# print(the_route_net[node])
+				if len(the_route_net[node]) == 1:
+					new_path.append(the_route_net[node][0]['seq_nb'])
+				else:
+					for k in range(len(the_route_net[node])):
+						if the_route_net[node][k]['node']== path[i+1]:
+							new_path.append(the_route_net[node][k]['seq_nb'])
+					# print(path[i+1])
+		result.append(new_path)
+	return result
+
+## Extrait une section du scenario basée sur son numéro
+def extract_section(selected_route, seq_nb):
+	for route in selected_route["route_paths"]:
+		id = route['id']
+		for section in route["route_sections"]:
+			if section["sequence_number"] == seq_nb:
+				result = section
+				result["id"] = id
+				return result 
+
+
+print("Je télécharge le scenario.")
 ## Upload le scenario
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 scenario = os.path.join(currentdir,'sample_files',"sample_scenario.json")
-
 with open(scenario) as fp:
 	scenario_content = json.load(fp)
 
-nb_routes = len(scenario_content['routes'])
+print("Je créé le réseau de routes et je liste les chemins possibles.")
+## Créé le réseau de routes
+the_route_net = generate_paths_dict(scenario_content)
+
+## Liste tous les chamins possibles
+paths = []
+for node in the_route_net:
+	myDFS(the_route_net,node,False,paths)
+final_paths = []
+for path in paths:
+	if path[0][-9:] == 'beginning':
+		final_paths.append(path)
+
+print("==> Il y a " + str(len(final_paths)) + " chemins possibles.")
+
+final_paths = convert_paths(final_paths, the_route_net)
+
 
 ## Extraction des paramètres globaux du scenario
 problem_instance_label = scenario_content['label']
 problem_instance_hash = scenario_content['hash']
-hash = 42
+hash = -516528637
 train_runs = []
+
 nb_of_trains = len(scenario_content['service_intentions'])
 
-## Boucle pour chaque train du scenario
+print("==> Il y a " + str(nb_of_trains) + " services différents à proposer.")
+
+list_of_solutions = []
 for k in range(nb_of_trains):
 	train = scenario_content['service_intentions'][k]
 	service_intention_id = train['id']
+	start_section = train["section_requirements"][0]
+	train_runs_possible = []
 
-	## Initialise les listes qui construiront la solution finale 
-	train_run_sections = []
-	sections = []
-	time = []
 	print("#################################")
 	print("service_intention_id ::: " + str(service_intention_id))
-	
-
-	#################################
-	##### IMPLEMENT SOLUTION ########
-	#################################
-
-	## Extrait les requirements de la première section du scenario
-	for j in range(len(train['section_requirements']) ):
-		if train['section_requirements'][j]['sequence_number'] == 1:
-			start_section = train['section_requirements'][j]
-
-	departs_possibles = []
-	intersections = {}
 
 	## Extrait la route correspondant au scenario
 	for route in scenario_content['routes']:
 		if route['id'] == service_intention_id:
 			selected_route = route 
 
-	## Loop à travers les sections de cette route
-	for path in selected_route["route_paths"]:
-		for (i, route_section) in enumerate(path["route_sections"]):
-			sn = route_section['sequence_number']
+	for chemin in final_paths:
+		print("Chemin testé : "+str(chemin))
+		## Initialise les listes qui construiront la solution finale 
+		train_run_sections = []
+		sections = []
+		time = []
+		for (idx, seq_nb) in enumerate(chemin):
+			section = extract_section(selected_route, seq_nb)
 
-			## Est-ce que cette section est un départ ?
-			if from_node_id(path, route_section, i)[-10:] == 'beginning)':
-				departs_possibles.append(route_section)
-
-			## Est-ce que cette section est suivi d'une intersection ? 
-			################ VERIFIER CE CODE #######################
-			# from_node ou to_node ?
-			elif from_node_id(path, route_section, i)[:2] == '(M':
-				if str(from_node_id(path, route_section, i)[1:-1]) not in intersections.keys():
-					intersections[str(from_node_id(path, route_section, i)[1:-1])] = []
-
-				intersections[str(from_node_id(path, route_section, i)[1:-1])].append(route_section)
-			#########################################################
-
-	## Choisir le meilleurs depart
-	for ii in range(len(departs_possibles)):
-		# Verifier aue c'est le bon section marker (A, B, ...)
-		if departs_possibles[ii]['section_marker'][0] != start_section['section_marker']:
-			print("Ce n'est pas le bon section_marker pour le point de depart !! ")
-			del departs_possibles[ii]
-
-	## Prendre le trajet le plus rapide
-	selected_depart = le_plus_court(departs_possibles)
-
-	## Ajouter la section selectionnée à la liste
-	sections.append(selected_depart)
-
-	## Ajouter les temps d'arrivée et de sortie de la gare
-	################ VERIFIER CE CODE #######################
-	# Pour la première le entry et exit devrait être le même en fait....
-	exit = datetime.datetime.strptime(start_section['entry_earliest'], "%H:%M:%S") +  \
-	isodate.parse_duration(selected_depart['minimum_running_time'])
-	time.append({
-		"entry_time": start_section['entry_earliest'],
-		"exit_time": exit.strftime("%H:%M:%S")
-	})
-	#########################################################
-
-	## Loop pour les sections suivantes
-	done = False
-	while not done:
-		## La dernière section selectionnée
-		current_section = sections[len(sections)-1]
-
-		## Est ce  que c'est section mène à une intersection ?
-		if "route_alternative_marker_at_exit" in current_section.keys() and \
-				route_section["route_alternative_marker_at_exit"] is not None:
-			possible_paths= intersections[route_section["route_alternative_marker_at_exit"][0]]
-			
-			if len(possible_paths) > 0:
-				# On choisit le temps de trajet le plus court
-				next_section = le_plus_court(possible_paths)
+			if idx == 0 :
+				if "entry_earliest" not in start_section.keys():
+					print("Je ne connais pas l'heure du départ pour la première séquence.")
+				else:
+					exit = datetime.datetime.strptime(start_section['entry_earliest'], "%H:%M:%S") +  \
+					isodate.parse_duration(section['minimum_running_time'])
+					new_time = {
+						"entry_time": start_section['entry_earliest'],
+						"exit_time": exit.strftime("%H:%M:%S")
+					}
 			else:
-				# Il n'y a qu'un choix possible et pis c'est tout
-				next_section = possible_paths
+				section_requirements = None
+				if "section_marker" in section.keys():
+					for r in train["section_requirements"]:
+						if r["section_marker"] == section["section_marker"][0]:
+							section_requirements = r
 
-			## Ajouter la section selectionnée à la liste
-			sections.append(next_section)
+				entry_time = time[len(time)-1]["exit_time"]
+				entry_time = datetime.datetime.strptime(entry_time, "%H:%M:%S")
+				new_time = compute_time(section_requirements, entry_time, section)
 
-			## Ajouter les temps d'arrivée et de sortie de la gare
-			new_time = compute_time(time[len(time)-1], next_section)
 			time.append(new_time)
-			print(next_section)
+			sections.append(section)
 
-		else:
-			################ CODER LA PARTIE OU IL N'Y A PAS D'INTERSECTION #######################
-			done = True
-			#######################################################################################
-		
-	
-	## On fait un joli dict pour chaque section
-	idx = 0
-	for section in sections:
-		sol = {
-			"entry_time": time[idx]["entry_time"],
-			"exit_time": time[idx]["exit_time"],
-			"route": service_intention_id,
-			"route_section_id": str(service_intention_id) +"#"+str(section["sequence_number"]),
-			"sequence_number": section["sequence_number"],
-			"route_path": 3,
-			"section_requirement": section["section_marker"]
-		}
+		## On vérifie qu'on passe bien par les gares des requirements
+		marker_to_go = []
+		for r in train["section_requirements"]:
+			marker_to_go.append(r["section_marker"])
 
-		
-		train_run_sections.append(sol)
+		idx = 0
+		for ss in sections:
+			try :
+				marker = ss["section_marker"][0]
+				if marker not in marker_to_go:
+					marker = None
+			except:
+				marker = None
+			
+			## On fait un joli dict pour chaque section
+			sol = {
+				"entry_time": time[idx]["entry_time"],
+				"exit_time": time[idx]["exit_time"],
+				"route": service_intention_id,
+				"route_section_id": str(service_intention_id) +"#"+str(ss["sequence_number"]),
+				"sequence_number": idx+1,
+				"route_path": ss['id'],
+				"section_requirement": marker
+			}
 
-		# on ajoute le joli dict à la liste
+			train_run_sections.append(sol)
+			idx += 1
+
 		result = {'service_intention_id': service_intention_id, 'train_run_sections': train_run_sections}
-		train_runs.append(result)
-		idx += 1
 
+		## La liste de toutes les runs possibles pour ce service_intention_id
+		train_runs_possible.append(result)
+
+	## La listes de toutes les solutions possibles
+	solution = {
+		"service_intention_id": service_intention_id,
+		"train_runs_possible": train_runs_possible
+	}
+	list_of_solutions.append(solution)
+
+
+for ii in range(len(list_of_solutions)):
+	print("==> Pour le service " + str(list_of_solutions[ii]['service_intention_id']) + 
+		", il y a " + str(len(list_of_solutions[ii]['train_runs_possible'])) +
+		" solutions possibles.")
+
+## On prends une solutions parmis les solutioons possibles pour chaque service_intention_id
+train_runs= [list_of_solutions[0]['train_runs_possible'][3],\
+	list_of_solutions[1]['train_runs_possible'][3]]
+	
 # le json final ! Ouhouuu !
 my_solution = {
 	'problem_instance_label': problem_instance_label,
@@ -183,6 +283,3 @@ my_solution = {
 # enregistré !!
 with open('my_solution.json', 'w') as fp:
 	json.dump(my_solution, fp, indent=4)
-
-################ ADAPTER VALIDATE_SOLUTION DE UTILS #######################
-###########################################################################
